70dc31205db27ed7f2750d6066c930c6f72bddf9
Kikemm11 <Kikemaldonado11@gmail.com>
2024-09-06 22:12:48 -0400

    Add Priority Selection (Non-Preemptive) algorithm and fix a bug with the execution time statistic

--- windows/SimulationWindow.py
+++ windows/SimulationWindow.py
@@ -92,6 +92,7 @@ class SimulationWindow(Toplevel):
             self.process_waiting_times = []
             self.finished_processes = []
             self.cpu_usage = []
+            self.execution_time = []
             
             self.current_process = None
             self.processes.sort(key=lambda x: x.arrival_time)
@@ -104,16 +105,47 @@ class SimulationWindow(Toplevel):
             pass
         elif self.selected_algorithm == "RandomSelection":
             pass
-        elif self.selected_algorithm == "PrioritySelection":
-            pass
+        elif self.selected_algorithm == "PrioritySelection (Non-Preemptive)":
+            
+            # Special variables for PrioritySelection (Non-Preemptive) algorithm
+            
+            self.simulation_time = 0.1
+            
+            self.arrival_time = 0
+            self.burst_time = 0
+            self.cpu_usage_time = 0
+            
+            self.cpu_available = True
+            
+            self.ready_processes = []
+            self.process_waiting_times = []
+            self.finished_processes = []
+            self.cpu_usage = []
+            self.execution_time = []
+            
+            self.current_process = None
+            self.processes.sort(key=lambda x: x.priority, reverse=True)
+            
+            # Function call
+            
+            self.priority_selection_non_preemptive()
+            
+        
         elif self.selected_algorithm == "RoundRobin":
             pass
         elif self.selected_algorithm == "SRTF":
             pass
+        elif self.selected_algorithm == "PrioritySelection (Preemptive)":
+            pass
 
 
 
-    # Non-Preemptive algorithms
+# ------------------------------- Non-Preemptive algorithms -------------------------------------------
+
+
+
+
+    # FIFO ALGORITHM
 
 
     def fifo(self):
@@ -133,6 +165,7 @@ class SimulationWindow(Toplevel):
 
 
         # Manage the logic when the cpu is available to accept a process
+        
 
         if self.ready_processes and self.cpu_available:
             
@@ -153,6 +186,7 @@ class SimulationWindow(Toplevel):
             self.finished_listbox.insert(tk.END, f"PID: {self.current_process.pid}, AT: {self.current_process.arrival_time}, BT: {self.current_process.burst_time}, PR: {self.current_process.priority}")
             self.cpu_available = True
             self.cpu_usage.append(self.cpu_usage_time)
+            self.execution_time.append(self.current_process.waiting_time + self.current_process.burst_time)
             self.finished_processes.append(self.current_process)
             self.current_process = None
             
@@ -168,7 +202,7 @@ class SimulationWindow(Toplevel):
         avrg_cpu_usage = (sum(self.cpu_usage) / self.simulation_time ) * 100
         self.stats_text.insert(tk.END, f"CPU usage %: {round(avrg_cpu_usage,2)}%\n")
         
-        avrg_execution_time = mean(self.cpu_usage) if self.cpu_usage else 0
+        avrg_execution_time = mean(self.execution_time) if self.execution_time else 0
         self.stats_text.insert(tk.END, f"Average Execution Time: {round(avrg_execution_time, 2)}\n")
         
         
@@ -197,3 +231,94 @@ class SimulationWindow(Toplevel):
         self.simulation_time += end_timestamp - start_timestamp
                 
         self.after(1, self.fifo)
+        
+        
+        
+    # PRIORITY SELECTION ALGORITHM     
+        
+        
+    def priority_selection_non_preemptive(self):
+        
+        if self.total_processes == len(self.finished_processes): return
+        
+        start_timestamp = time.time()
+        
+        # Update process queue and add process to the ready_process list
+        
+        self.queue_listbox.delete(0, tk.END)
+        for process in self.processes:
+            if  self.arrival_time >= process.arrival_time:    
+                self.queue_listbox.insert(tk.END, f"PID: {process.pid}, AT: {process.arrival_time}, BT: {process.burst_time}, PR: {process.priority}")
+                if process not in self.ready_processes:
+                    self.ready_processes.append(process)
+                    
+        
+        # Manage the logic when the cpu is available to accept a process
+        
+        self.ready_processes.sort(key=lambda x: x.priority, reverse=True)
+
+        if self.ready_processes and self.cpu_available:
+            
+            self.cpu_available = False
+            self.burst_time = 0
+            self.cpu_usage_time = 0
+            if self.processes: self.processes.remove(self.ready_processes[0])
+            
+            self.current_process = self.ready_processes.pop(0)
+            self.process_waiting_times.append(self.current_process.waiting_time)
+            self.current_process_value.config(text=f"PID: {self.current_process.pid}")
+            self.queue_listbox.delete(0)
+            
+        
+        # Check if there is a current process in the cpu and if it has completed its burst time
+        
+        if self.current_process and self.burst_time >= self.current_process.burst_time:
+            self.finished_listbox.insert(tk.END, f"PID: {self.current_process.pid}, AT: {self.current_process.arrival_time}, BT: {self.current_process.burst_time}, PR: {self.current_process.priority}")
+            self.cpu_available = True
+            self.cpu_usage.append(self.cpu_usage_time)
+            self.execution_time.append(self.current_process.waiting_time + self.current_process.burst_time)
+            self.finished_processes.append(self.current_process)
+            self.current_process = None
+            
+        # Set the cpu label to None if it has no process    
+            
+        if not self.current_process:
+            self.current_process_value.config(text="None")
+            
+        
+        # Update the statistics    
+            
+        self.stats_text.delete(1.0, tk.END)
+        
+        avrg_cpu_usage = (sum(self.cpu_usage) / self.simulation_time ) * 100
+        self.stats_text.insert(tk.END, f"CPU usage %: {round(avrg_cpu_usage,2)}%\n")
+        
+        avrg_execution_time = mean(self.execution_time) if self.execution_time else 0
+        self.stats_text.insert(tk.END, f"Average Execution Time: {round(avrg_execution_time, 2)}\n")
+        
+        
+        avrg_waiting_time = mean(self.process_waiting_times) if self.process_waiting_times else 0
+        self.stats_text.insert(tk.END, f"Average Waiting Time: {round(avrg_waiting_time, 2)}\n")
+        
+        self.stats_text.insert(tk.END, f"Total processes completed: {len(self.finished_processes)}\n")
+        
+        self.stats_text.insert(tk.END, f"Simulation time: {round(self.simulation_time, 2)} seconds\n")
+        
+        self.update_idletasks()
+        
+        # Update the timers
+        
+        end_timestamp = time.time()  
+        
+        self.arrival_time += end_timestamp - start_timestamp
+        self.burst_time += end_timestamp - start_timestamp
+        
+        for process in self.ready_processes:
+            process.waiting_time += end_timestamp - start_timestamp
+            
+        if not self.cpu_available:
+            self.cpu_usage_time += end_timestamp - start_timestamp
+            
+        self.simulation_time += end_timestamp - start_timestamp
+                
+        self.after(1, self.priority_selection_non_preemptive)
